273b373f312c1155f8922d86c6d7cd926b7fe971
WernerNamDo <wernernam.do@hotmail.com>
2022-11-10 16:02:21 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("61a15aad-0722-4237-9ee6-bef4c650135d");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
